49b0c98a8b999fea0ba6c7358c5b329ff2dd80be
Aryan Dadwal <aryandadwal2006@gmail.com>
2025-11-02 20:37:29 +0530

    Add files via upload

---
+++ install_requirements.py
@@ -0,0 +1,5 @@
+"""
+to install required packages, run this after creating virtual env
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install pytorch-forecasting pytorch-lightning tensorboard scikit-learn pandas numpy
+"""
